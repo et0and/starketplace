cf4d899c2e553e0c8378c8e0ef6c9b9fd15d9851
Tom Hackshaw <et0and@users.noreply.github.com>
2022-01-29 19:15:36 +1300

    would this work?

--- src/components/Make.tsx
+++ src/components/Make.tsx
@@ -7,8 +7,12 @@ export default function Make() {
             <HomeHeader />
 
             <section className="layout">
-            <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"></script>
-            <iframe class="airtable-embed airtable-dynamic-height" src="https://airtable.com/embed/shr4vQsrJ4hCqmd4K?backgroundColor=white" frameborder="0" onmousewheel="" width="100%" height="1621" style="background: transparent; border: 1px solid #fff;"></iframe>
+            
+class App extends React.Component {
+  render() {
+    return <iframe src="https://airtable.com/embed/shr4vQsrJ4hCqmd4K?backgroundColor=white" />;
+    }
+} 
             </section>
 
             <Footer />
