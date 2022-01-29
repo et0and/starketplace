03734943027164345d16eeb64bce64031de6ac13
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2022-01-29 18:35:24 +1300

    Will added spacing work?

--- src/components/Header/HomeHeader.tsx
+++ src/components/Header/HomeHeader.tsx
@@ -8,7 +8,7 @@ export default function HomeHeader() {
                     <span className="ml-0.5em mr-0.5em">Te Wāhi Auaha</span>
                 </Link>
                 <nav>
-                    <Link to="/about" className='pill-button'>
+                    <Link to="/about" className='pill-button ml-0.6em mr-0.6em'>
                         About
                     </Link>
                     <a href="https://learn.tewahi.com" className="pill-button bg-yellow ml-0.6em mr-0.6em">
