c90eb5de98da1a0d5efb892add20a479425f8a72
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2022-01-29 17:54:07 +1300

    Wrote out PP

--- src/components/Disclaimer.tsx
+++ src/components/Disclaimer.tsx
@@ -8,17 +8,67 @@ export default function Disclaimer() {
 
             <section className="layout">
                 <div className="measure">
-                Disclaimer
+                Privacy Policy
 
-<h4>Urbit Wrapped Star (WSTR) Contract and Interface Disclaimer</h4>
+<p>Te Wāhi Auaha Makerspace ('Te Wāhi,' 'we,' 'our,' and/or 'us') values the privacy of individuals who use our websites and facilities (collectively, our 'Services'). This privacy policy (the 'Privacy Policy') explains how we collect, use, and share information from users of our Services ('Users'). By using our Services, you understand and agree to the collection, use, disclosure, and procedures this Privacy Policy describes. Beyond the Privacy Policy, your use of our Services is also subject to our Terms of Service.</p>
 
-<p>Urbit is an open-source peer-to-peer network, virtual computer, and identity system. Urbit identities are instantiated as ERC-721 tokens on the Ethereum blockchain. Urbit Wrapped Star Contract (“The Contract”) is a decentralized smart contract that can be used to deposit Urbit ERC-721 tokens, and receive “wrapped” ERC-20 tokens, and vice versa. The Contract is made up of free, public, open-source or source-available software that is deployed on the Ethereum Blockchain. The Urbit Wrapped Star Contract Interface (“The Interface”) is a website that allows users to interact with The Contract.</p>
+<h4>Your privacy matters</h4>
 
-<p>Your use of the The Interface and The Contract involves various risks, including, but not limited to, losses while digital assets are being supplied to The Contract, and losses due to the fluctuation of prices of tokens you have exchanged. Before using The Interface or The Contract, you should review the relevant documentation to make sure you understand how they work. Additionally, just as you can access email email protocols such as SMTP through multiple email clients, you can potentially access The Contract through various web or mobile interfaces. Tlon Corporation is not responsible for these various interfaces. You are responsible for doing your own diligence on those interfaces to understand the fees and risks they present.</p>
+<p>At Te Wāhi, we care about your privacy and believe that ownership, privacy and control don’t need to be sacrificed in exchange for usability, accessibility and reliability. That’s why we limit our collection and use of your personally identifiable information (“PII”) to what’s necessary to provide our Services to you, unless you give us your consent.</p>
 
-<p>AS DESCRIBED IN THE INTERFACE AND CONTRACT LICENSES, THEY ARE EACH SEPARATELY  PROVIDED ”AS IS”, AT YOUR OWN RISK, AND WITHOUT WARRANTIES OF ANY KIND. Although Tlon Corporation developed much of the initial code for The Interface, it does not provide, own, or control The Contract, which is deployed on the Ethereum blockchain, and was developed by third parties not related to Tlon. Upgrades and modifications to The Contract are managed in a community-driven way by the developers of The Contract. No developer or entity involved in creating the Interface, or The Contract will be liable for any claims or damages whatsoever associated with your use, inability to use, or your interaction with other users of The Contract or The Interface, including any direct, indirect, incidental, special, exemplary, punitive or consequential damages, or loss of profits, cryptocurrencies, tokens, or anything else of value.</p>
+<h4>Information we collect</h4>
 
- 
+<p>We may collect a variety of information from or about you or your devices from various sources, as described below. We collect information: 1) when you provide it to us or give us permission to collect it; and 2) automatically, when you interact with the Services.</p>
+
+<p>1. Maker Request Information. <br />
+    When you request a to create a project at Te Wāhi, you voluntarily provide us with certain information, such as your name, student ID and email. You may also provide us with your project files associated with what you are building.</p>
+
+<p>2. Communications. <br />
+    If you contact us directly, we may receive additional information about you. For example, when you chat with us via email, we may receive your student ID, the contents of a message or attachments that you may send to us, and other information you choose to provide.</p>
+
+<p>3. Analytics. <br />
+
+When you use or interact with our Services, certain information is automatically collected and logged. We collect information automatically when you:</p>
+
+<p>visit, interact with, or use Te Wāhi Auaha online, Te Wāhi Learn or other Offerings (including when you use your computer or other device to interact with Te Wāhi);<br />
+open emails or click on links in emails from us; and interact or communicate with us (such as when you visit us in person or when you request support via email). Here are some types of information we automatically collect:<br />
+Log Data. When you visit, interact with, or use our Services, our servers record information automatically sent by your browser or device (“log data”). This log data includes your Internet Protocol address, browser type and settings, the data and time of your interactions, how you used our Services, cookie data, device data, occurrences of technical errors, diagnostic reports, your settings preferences, backup information, and API calls.<br />
+Device Information. We receive information about the device and software you use to access our websites, including IP address, unique device identifier, web browser type and version, device model, and operating system version. We may infer your location based on your IP address.<br />
+Clickstream Data. To help us understand how you use our Services and to help us improve them, if you let us, we automatically receive information about your interactions with the Services, like the full Uniform Resource Locators (URL) clickstream to, through, and from our website, pages or other content you viewed or searched for, the dates and times of your visits. Page response times, download erros, and page interaction information (such as scrolling, clicks, and mouse-overs)<br />
+Information from Cookies and Similar Technologies. On our websites, we and third party partners collect information using cookies, or similar technologies. Our third party partners, such as analytics partners like Google Analytics, may use these technologies to collect information about your online activities over time and across different online services. Cookies are small text files containing a string of alphanumeric characters. We may use both session cookies and persistent cookies. A session cookie disappears after you close your browser. A persistent cookie remains after you close your browser and may be used by your browser on subsequent visits to our website. Please review your web browser 'Help' file to learn the proper way to modify your cookie settings. In particular, please visit https://policies.google.com/technologies/partner-sites to learn more about how Google uses data.
+</p>
+
+<h4>How we use the information we collect</h4>
+
+<p>We use the information we collect to provide our Services to you, to improve your experience, to comply with our obligations and to keep you safe. We have a legitimate interest to collect and use your information in these ways, including to:</p>
+
+<p>Identify you or your device when you use the Service<br />
+To improve and enhance our Services and develop new products, services, features, and functionality;<br />
+To understand and analyze how you use our Services<br />
+Respond to your questions or comments when you reach out to us;<br />
+To find and prevent fraud;<br />
+To respond to trust and safety issues that may arise; and<br />
+For compliance purposes,  as may be required by applicable laws and regulations or requested by any judicial process or governmental agency.<br />
+</p>
+
+<h4>How we share the information we collect</h4>
+
+<p>We do not share or otherwise disclose information we collect from or about you except as described below or otherwise disclosed to you at the time of the collection.</p>>
+
+<p>Analytics Partners. We use analytics services such as Google Analytics to collect and process certain analytics data. These services may also collect information regarding your use of other websites, apps, and online resources. You can learn about Google's practices by going to https://www.google.com/policies/privacy/partners/, and opt out of them by downloading the Google Analytics opt-out browser add-on, available at https://tools.google.com/dlpage/gaoptout.<br />
+
+As Required By Law and Similar Disclosures. We may access, preserve, and disclose your information if we believe doing so is required or appropriate to (a) comply with law enforcement requests and legal process, such as a court order or subpoena; (b) respond to your requests; or (c) protect your, our or others' rights, property, or safety. For the avoidance of doubt, the disclosure of your information may occur if you post any objectionable content on or through the Service.</p>
+
+<h4>Third parties</h4>
+
+<p>The Services may contain links to other websites, products, or services that Te Wāhi does not own or operate. We are not responsible for the privacy practices of these third parties. We encourage you to read their privacy policies before providing any information to them.</p>
+
+<p>We make reasonable efforts to protect your information by using physical and electronic safeguards designed to improve the security of the information we maintain. However, as our Services are hosted electronically, we can make no guarantees as to the security or privacy of your information.</p>
+
+<h4>Changes to this privacy policy</h4>
+
+<p>We will post any adjustments to the Privacy Policy on this page, and the revised version will be effective when it is posted. If we materially change the ways in which we use or share personal information previously collected from you through the Services, we will notify you through the Services, by email, or other communication.</p>
+
             </div>
             </section>
             <Footer/>
