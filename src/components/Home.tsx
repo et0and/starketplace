b07ec36d12ea86a8c71cb1b6b7b924309bc016c0
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2023-04-11 19:13:13 +1200

    Added some more links

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -78,7 +78,7 @@ export default function Home() {
                         </h2>
                         <p>Te Wāhi uses many different kinds of free and open source software that can be run on multiple platforms. Jump in to get learning about these tools and use them yourself.
                         </p>
-                            <a rel="noreferrer" target="_blank" href="https://learn.tewahi.com" className="inline-block mt-1 button-sm button-bg-black">Learn More about tools</a>
+                            <a rel="noreferrer" target="_blank" href="https://learn.tewahi.com" className="inline-block mt-1 button-sm button-bg-black">Learn more about tools</a>
                     </div>
                     <div style={{
                         backgroundImage:"url('/assets/printer.jpg')",
@@ -102,7 +102,7 @@ export default function Home() {
                         <p>Get started right away with a template project that you can freely customise to your liking. They can be made as simple or complex as you like, and provide a great starting point for learning about the different tools we have available.
                        
                         </p>
-                            <a rel="noreferrer" target="_blank" href="https://learn.tewahi.com/projects/digitalkaitiaki" className="inline-block mt-1 button-sm button-bg-black">Learn More about projects</a>
+                            <a rel="noreferrer" target="_blank" href="https://learn.tewahi.com/projects/digitalkaitiaki" className="inline-block mt-1 button-sm button-bg-black">Learn more about projects</a>
                     </div>
                     <div style={{
                         backgroundImage:"url('/assets/project.jpg')",
@@ -126,11 +126,36 @@ export default function Home() {
                         <p>Explore past and present virtual exhibitions hosted by Te Wāhi, or learn more about any upcoming events you can participate in.
                         </p>
                         <a rel="noreferrer" target="_blank" href="https://exhibition.tewahi.com" className='inline-block mt-1 button-sm button-bg-black'>
-                        Learn More about exhibitions
+                        Learn more about exhibitions
                         </a>
                     </div>
                     <div style={{
                         backgroundImage:"url('/assets/exhibition.jpg')",
+                        backgroundSize:"cover",
+                        backgroundPosition:"center",
+                        width: '300px',
+                        height: '300px',
+                        marginTop:'1em'
+                    }}
+                    className="home-section-img ml-1em" />
+                </div>
+            </section> 
+
+            <section className="layout">
+                <div className="flex justify-between">
+                    <div className="measure mr-1em">
+                        <h3 className="gray-title">Visual Arts Innovation</h3>
+                        <h2>
+                            Index of some 10VAI work
+                        </h2>
+                        <p>Browse an archive of past student work, made as part of the Year 10 Visual Arts Innovation course at Takapuna Grammar School.
+                        </p>
+                        <a rel="noreferrer" target="_blank" href="https://vai.tewahi.com" className='inline-block mt-1 button-sm button-bg-black'>
+                        Enter the archive
+                        </a>
+                    </div>
+                    <div style={{
+                        backgroundImage:"url('/assets/vai.jpg')",
                         backgroundSize:"cover",
                         backgroundPosition:"center",
                         width: '300px',
